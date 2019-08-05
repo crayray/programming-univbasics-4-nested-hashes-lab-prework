8a5943ad61dc02d580a04cf96c78846002e78bba
Rachel Fuerst <rach.fuerst@gmail.com>
2019-08-05 21:45:43 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -90,7 +90,8 @@ end
 
 def changing_alan
 	programmer_hash =
- 		{ :grace_hopper => {
+ 		{ 
+			:grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         },
@@ -104,7 +105,7 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     alans_new_info = "GUI"
+    #  alans_new_info = "GUI"
 
 		#  programmer_hash[:alan_kay][:known_for] = "GUI"
 
