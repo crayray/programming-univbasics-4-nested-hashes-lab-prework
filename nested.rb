aa1d422d0a2059e51a98f4649dcb0a8007c96f93
Rachel Fuerst <rach.fuerst@gmail.com>
2019-08-05 21:28:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -88,7 +88,7 @@ def adding_matz
 
 end
 
-def changing_alan()
+def changing_alan
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -107,7 +107,7 @@ def changing_alan()
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
 
-		 programmer_hash[:alan_kay][:known_for] = "GUI"
+		 programmer_hash[:alan_kay][:known_for] = alans_new_info
 		 programmer_hash[:alan_kay][:known_for]
 
 end
