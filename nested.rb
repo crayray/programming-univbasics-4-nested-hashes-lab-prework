0bb66c68221eb583e0a81e28f18e3a4bcd27fd9d
Rachel Fuerst <rach.fuerst@gmail.com>
2019-08-05 20:50:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		programmer_hash[:dennis_ritchie][:launguages][0]
+		programmer_hash[:dennis_ritchie][:launguages]
 end
 
 def adding_matz
